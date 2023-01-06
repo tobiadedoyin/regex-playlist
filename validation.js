6053aeedada339a994bb14a0da7338b1c7c9ee2a
Tobi OLADELE <oladeletobiadedoyin@gmail.com>
2023-01-06 22:20:19 +0100

    my regex practise

--- validation.js
+++ validation.js
@@ -1,1 +1,24 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z0-9]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/i,
+  slug: /^[\da-z-]{8,20}$/,
+  email: /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{2,8})(\.[a-z]{2,5})?$/,
+};
+
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
